a1ec6229a50b4aa3f1ad2ac8c09d5ecb89a71cba
Alejandro <alejandro.mascort17@hotmail.com>
2021-07-10 07:22:22 +0200

    avoid every repository to be specified as a service by setting arguments for AbstractDoctrineRepository

--- app/src/Chaplin/Quote/Infrastructure/Repository/QuoteRepositoryDoctrine.php
+++ app/src/Chaplin/Quote/Infrastructure/Repository/QuoteRepositoryDoctrine.php
@@ -5,17 +5,10 @@ namespace Chaplin\Quote\Infrastructure\Repository;
 use Chaplin\Core\Infrastructure\AbstractDoctrineRepository;
 use Chaplin\Quote\Domain\Entity\Quote;
 use Chaplin\Quote\Domain\Repository\QuoteRepository;
-use Doctrine\ORM\EntityManager;
-use JetBrains\PhpStorm\Pure;
 
 class QuoteRepositoryDoctrine extends AbstractDoctrineRepository implements QuoteRepository
 {
     private const ALIAS = 'quote';
-
-    #[Pure] public function __construct(EntityManager $entityManager)
-    {
-        parent::__construct($entityManager);
-    }
 
     public function findAll(): array
     {
